644cdbfdadf0951d1adea7f0d78aca4c7efaaed9
Edwin <edipopm@gmail.com>
2024-07-30 20:48:56 -0400

    third commit

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,8 +4,47 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Plan;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Carbon\Carbon;
 
 class Account extends Model
 {
     use HasFactory;
+
+    public function plan(): BelongsTo
+    {
+    	return $this->belongsTo(Plan::class);
+    }
+
+    protected function daysElapsed(): Attribute
+    {
+    	return Attribute::make(
+    		get: fn() => Carbon::create($this->started_at)->diffInDays(now()),
+    	);
+    }
+
+    protected function daysRemaining(): Attribute
+    {
+    	return Attribute::make(
+    		get: fn() => Carbon::now()->diffInDays($this->finished_at),
+    	);
+    }
+
+    protected function totalDays(): Attribute
+    {
+    	return Attribute::make(
+    		get: fn() => Carbon::create($this->started_at)->diffInDays($this->finished_at),
+    	);
+    }
+    
+    protected function progress(): Attribute
+    {
+    	return Attribute::make(
+    		get: fn() => number_format(($this->days_elapsed/($this->total_days) * 100) / 10) * 10,
+    	);
+    }
+
+
 }
--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Account;
 
 class Plan extends Model
 {
     use HasFactory;
+
+    public function accounts(): HasMany
+    {
+    	return $this->hasMany('Account::class');
+    }
 }
